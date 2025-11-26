ad192ba2684bfb9bf8695b7da2d0810b533e1c4b
Rapha1111 <raphaa.anis@gmail.com>
2025-11-26 15:52:17 +0100

    added photo + sitemap + robots

---
+++ blur.js
@@ -0,0 +1,19 @@
+function updateBlur() {
+  const scroll = window.scrollY;
+  const maxScroll = document.body.scrollHeight - window.innerHeight;
+
+  // ratio entre 0 et 1
+  const progress = scroll *2/ maxScroll;
+
+  // flou max : 30px
+  const blur = Math.min(30 * progress, 30) + "px";
+
+  // opacité max de la teinte : 0.85
+  const tint = Math.min(0.85 * progress, 1);
+
+  document.body.style.setProperty("--blur-amount", blur);
+  document.body.style.setProperty("--tint-opacity", tint);
+}
+
+window.addEventListener("scroll", updateBlur);
+updateBlur();
